88d14e66108ab0a6771bf3bca5f53339f2ff3f89
Evan Juszczak <evanjuszczak@Evans-MacBook-Air.local>
2025-02-14 11:49:02 -0600

    fix: Update build configurations and dependency versions

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,15 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          ui: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled']
+        }
+      }
+    }
   }
 })
